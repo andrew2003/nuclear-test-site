fba7821a795ef67c9b5b9dedd4d627df35b580e9
Tran Anh Dung <trananhdung280103@gmail.com>
2024-12-03 18:53:26 +0700

    feat: test commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
 fn main() {
-    println!("Test git 5!");
-    println!("Test git 2 --1  -3!");
-    println!("Test git 2 --2  -3!");
-    println!("Test git 2 --3  -3!");
+    println!("Test git 3!");
 }
